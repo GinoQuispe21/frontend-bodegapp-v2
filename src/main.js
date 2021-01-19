d688c67d4f302d8316baba3b70032da7a01cd7be
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-01-19 13:21:20 -0500

    Created component Login.vue :sparkles:

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "@/router";
+import bootstrap_vue from 'bootstrap-vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 Vue.config.productionTip = false
+Vue.use(bootstrap_vue)
+Vue.use(VueAxios, axios)
 
 new Vue({
+  router,
+  bootstrap_vue,
   render: h => h(App),
 }).$mount('#app')
