20da412791d8122335790ebe8764dacd6d536f5f
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-02-03 17:13:49 -0500

    Finished the design of all of the registry components. :zap:

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -13,28 +13,32 @@
                         <h3>First time here? Sign up to us!</h3>
                         <form>
                             <div class="form-row">
-                                <div class = "col-lg-7">
+                                <div class = "col-lg-8">
+                                    <p>Write your email:</p>
                                     <input type = "email" placeholder ="Email-Address" class ="form-control my-3 p-4">
                                 </div>
                             </div>
                             <div class="form-row">
-                                <div class = "col-lg-7">
+                                <div class = "col-lg-8">
+                                    <p>Write your password:</p>
                                     <input type = "password" placeholder ="Password" id = "myPassword" class ="form-control my-3 p-4">
                                     <input type="checkbox" @click="showPassword"> Show Password
                                 </div>
                             </div>
+                            <br>
                             <div class="form-row">
-                                <div class = "col-lg-7">
+                                <div class = "col-lg-8">
+                                    <p>Please, confirm your password:</p>
                                     <input type = "password" placeholder ="Confirm-Password" id = "myCPassword" class ="form-control my-3 p-4">
                                     <input type="checkbox" @click="showPassword"> Show Password
                                 </div>
                             </div>
                             <div class="form-row">
-                                <div class = "col-lg-7">
-                                    <router-link to="/register2" class ="go-register2"><button type ="button" class = "register mt-3 mb-5">Register</button></router-link>
+                                <div class = "col-lg-8">
+                                    <router-link to="/register2" class ="go-register2"><button type ="button" class = "register mt-3 mb-4">Continue</button></router-link>
+                                    <p>Already have an account? <router-link to="/" class ="back-login mt-3">Sign in here</router-link></p>
                                 </div>
                             </div>
-                            <p>Already have an account? <router-link to="/" class ="back-login">Sign in here</router-link></p>
                         </form>
                     </div>
                     <div class = "col-lg-5">
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import router from "@/router";
 import bootstrap_vue from 'bootstrap-vue'
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 Vue.use(bootstrap_vue)
