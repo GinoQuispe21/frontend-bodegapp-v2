65179da45f9ad1e0ed5984e06bc53056384c1cfa
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-02-08 19:25:15 -0500

    Reinstall vuetify.js :heavy_minus_sign: :heavy_plus_sign:

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import vuetify from 'vuetify'
 import App from './App.vue'
 import router from "@/router";
 import bootstrap_vue from 'bootstrap-vue'
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import vuetify from './plugins/vuetify';
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
@@ -13,12 +13,12 @@ Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 Vue.use(bootstrap_vue)
 Vue.use(VueAxios, axios)
-Vue.use(vuetify)
 
 new Vue({
   router,
   bootstrap_vue,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
 
 
