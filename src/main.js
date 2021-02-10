f6beed92751fa7a345c2130299b5b1544d3adbe5
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-02-09 20:39:17 -0500

    First Impressions using SweetAlert2 on Vue.Js :art:

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import vuetify from './plugins/vuetify';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
@@ -13,6 +14,7 @@ Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 Vue.use(bootstrap_vue)
 Vue.use(VueAxios, axios)
+Vue.use(VueSweetalert2)
 
 new Vue({
   router,
