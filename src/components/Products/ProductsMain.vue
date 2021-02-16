8fd343537db0a6737f461d17a666da72cf9103a7
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-02-15 22:08:31 -0500

    Added table to show the products connected with the database :zap:

--- src/components/Products/ProductsMain.vue
+++ src/components/Products/ProductsMain.vue
@@ -1,11 +1,35 @@
 <template>
     <v-card>
         <h2>Products</h2>
-    </v-card>  
+        <v-data-table :headers="columns" :items="products" class="elevation-5">
+        </v-data-table>
+    </v-card>
+    
 </template>
 
 <script>
+import { baseURL } from '@/baseURL';
+
 export default {
+    name: "Products",
+    data() {
+        return{
+            columns : [
+                {text: 'ID', value: 'id', class:'blue darken-2 white--text'},
+                {text: 'PRODUCT', value:'productName', class:'blue darken-2 white--text'},
+                {text: 'PROVIDER', value:'providerName', class:'blue darken-2 white--text'},
+                {text: 'SALE PRICE', value:'salePrice', class:'blue darken-2 white--text'},
+                {text: 'PURCHASE PRICE', value:'purchasePrice', class:'blue darken-2 white--text'}
+            ],
+            products:[]
+        }
+    },
+    mounted(){
+        this.axios.get(baseURL + 'products').then(res =>{
+            console.log(res);
+            this.products = res.data.content;
+        })
+    }
     
 }
 </script>
