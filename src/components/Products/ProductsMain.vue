d38d913e6efb177d20c6c89034ab8ed32ee36420
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-02-17 13:32:04 -0500

    Improved component product  :art: :zap:

--- src/components/Products/ProductsMain.vue
+++ src/components/Products/ProductsMain.vue
@@ -1,16 +1,20 @@
 <template>
     <v-card>
-        <div>
-            <p></p>
-        </div>
         <div style="height: 1010px" class="xd">
+            <h2 class="title-component">List of Products</h2>
             <v-data-table 
                 :headers="columns" 
-                :items="products" 
+                :items="products"
+                :search="search" 
                 class="elevation-5">
                 <template v-slot:top>
                     <v-toolbar flat>
-                         <v-toolbar-title style="text-align:center">PRODUCTS</v-toolbar-title>
+                        <div class="search">
+                            <v-col cols="12" sm="12">
+                                <v-text-field v-model="search" append-icon="search" label="Serach Product" single-line hide-details></v-text-field>
+                            </v-col>
+                        </div>
+                        <v-divider class="mx-4" inset vertical></v-divider>
                         <v-spacer></v-spacer>
                         <v-btn class="mb-2" :elevation="10" color="brown darken-3" dark @click="drawer =!drawer">
                             New Product
@@ -18,8 +22,10 @@
                     </v-toolbar>
                 </template>
                 <template v-slot:[`item.actions`]="{ item }">
-                    <v-icon small @click="deleteItem(item.id)">mdi-delete</v-icon>
+                    <v-icon small class="mr-2" @click="editProduct(item.id)"> mdi-pencil </v-icon>
+                    <v-icon small @click="deleteProduct(item.id)"> mdi-delete </v-icon>
                 </template>
+                
             </v-data-table>
             <v-dialog v-model="drawer" max-width="500px">
                 <v-card>
@@ -47,13 +53,51 @@
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn color="#F87575" class="ma-2 white--text" @click="cancelar">Cancel</v-btn>
-                        <v-btn color="#778DA9" class="ma-2 white--text" @click="guardar">Register</v-btn>
+                        <v-btn color="#778DA9" class="ma-2 white--text" @click="register">Register</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
-            <v-col col="12" sm="12">
-                <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
-            </v-col>
+            <v-dialog v-model="dialogEdit" max-width="500px">
+                <v-card>
+                    <v-card-title class="brown darken-3">
+                        <span class="headline" style="color:white">Edit Product</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" sm="6">
+                                    <v-text-field label="Product Name*" required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <v-text-field label="Provider Name*" required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <v-text-field label="Sale Price*" required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <v-text-field label="Purchase Price*" required></v-text-field>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="#F87575" class="ma-2 white--text" @click="cancel">Cancel</v-btn>
+                        <v-btn color="#778DA9" class="ma-2 white--text" @click="save">Save</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="550px">
+                <v-card>
+                    <v-card-title class="headline">Are you sure you want to delete this product?</v-card-title>
+                    <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                    <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
 
         </div>
     </v-card>
@@ -67,8 +111,11 @@ export default {
     data() {
         return{
             drawer: false,
+            search: '',
+            dialogEdit: false,
+            dialogDelete: false,
             columns : [
-                {text: 'ID', value: 'id', class:'blue-grey darken-1 white--text'},
+                {text: 'ID', align: 'left',sortable: false, value: 'id', class:'blue-grey darken-1 white--text'},
                 {text: 'PRODUCT', value:'productName', class:'blue-grey darken-1 white--text'},
                 {text: 'PROVIDER', value:'providerName', class:'blue-grey darken-1 white--text'},
                 {text: 'SALE PRICE', value:'salePrice', class:'blue-grey darken-1 white--text'},
@@ -82,10 +129,27 @@ export default {
         this.axios.get(baseURL + 'products').then(res =>{
             this.products = res.data.content;
         })
-    },
+    },  
     methods: {
-        deleteItem(id){
-            console.log(id);
+        editProduct (item) {
+            console.log(item.id);
+            this.dialogEdit = true;
+        },
+        deleteProduct (item) {
+            console.log(item.id);
+            this.dialogDelete = true;
+        },
+        closeDelete () {
+            this.dialogDelete = false;
+        },
+        deleteItemConfirm () {
+            this.closeDelete();
+        },
+        cancel (){
+            this.dialogEdit = false;
+        },
+        save (){
+            this.dialogEdit = false;
         }
     }
     
@@ -104,6 +168,7 @@ export default {
     width:90%;
     position: relative;
     left: 85px;
+    top: 50px;
 }
 .plus{
     position: relative;
@@ -117,6 +182,16 @@ export default {
 .save{
     position: relative;
     left: 230px;
+}
+
+.search{
+    width: 1100px;
+}
+
+.title-component{
+    text-align: center;
+    position: relative;
+    top: 20px;
 }
 
 </style>
